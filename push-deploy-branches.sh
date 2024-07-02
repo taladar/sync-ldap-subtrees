8b1ef5ef279d4c054e24c7aff72a1778c046f881
Matthias Hörmann <matthias.hoermann@saltation.com>
2024-07-02 16:48:50 +0200

    remove outdated deploy branches and rpm spec files

--- push-deploy-branches.sh
+++ push-deploy-branches.sh
@@ -2,7 +2,7 @@
 
 set -e -u
 
-for b in centos6 centos7 centos8 stretch buster bullseye bookworm
+for b in centos7 buster bullseye bookworm
 do
   echo "Pushing build_${b}" >&2
   git push origin HEAD:refs/heads/build_${b}

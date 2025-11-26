51a5d1d573f57cf6ac4d5770ee0f13407efac6bf
Matthias Hörmann <matthias.hoermann@saltation.com>
2025-11-26 16:43:08 +0100

    build(deploy): Add trixie deploy branch

--- push-deploy-branches.sh
+++ push-deploy-branches.sh
@@ -2,7 +2,7 @@
 
 set -e -u
 
-for b in buster bullseye bookworm; do
+for b in buster bullseye bookworm trixie; do
   echo "Pushing build_${b}" >&2
   git push origin HEAD:refs/heads/build_${b}
 done
